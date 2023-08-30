c7b6af3ed2d55e2ced14192052bda0e2aefc8a82
Sayuru Akash Amarasinghe <sayuru555@gmail.com>
2023-08-30 07:05:36 +0530

    header and footer

--- next-app/src/app/layout.tsx
+++ next-app/src/app/layout.tsx
@@ -1,6 +1,8 @@
 import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import Header from "./header";
+import Footer from "./footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +18,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`px-12 ${inter.className}`}>{children}</body>
+      <body className={`px-12 ${inter.className}`}>
+        <Header />
+        {children}
+        <Footer />
+      </body>
     </html>
   );
 }
