f0a8018cce70d539bed752e59de483d7585056f3
Sayuru Akash Amarasinghe <sayuru555@gmail.com>
2025-11-19 03:23:24 +0530

    chore: update dependencies and devDependencies in package.json
    - Updated FontAwesome packages to version 6.7.1 and 0.2.2
    - Upgraded autoprefixer to version 10.4.20
    - Updated Next.js to version 15.0.3
    - Upgraded nodemailer to version 7.0.10
    - Updated postcss to version 8.4.49
    - Upgraded React and ReactDOM to version 19.0.0
    - Updated react-lite-youtube-embed to version 2.4.0
    - Upgraded swiper to version 11.1.15
    - Updated tailwindcss to version 3.4.15
    - Updated devDependencies:
      - @types/node to version 22.9.1
      - @types/nodemailer to version 6.4.17
      - @types/react and @types/react-dom to version 19.0.1
      - eslint to version 9.14.0
      - eslint-config-next to version 15.0.3
      - prettier to version 3.3.3
      - typescript to version 5.6.3
    
    refactor: change TypeScript target to ES2017 in tsconfig.json
    
    feat: add next.config.ts for image remote patterns configuration

---
+++ next-app/next.config.ts
@@ -0,0 +1,18 @@
+import type { NextConfig } from 'next';
+
+const nextConfig: NextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'img.freepik.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 's3-us-west-2.amazonaws.com',
+      },
+    ],
+  },
+};
+
+export default nextConfig;
